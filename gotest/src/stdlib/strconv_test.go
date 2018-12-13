984fe777a55149ed65d0a4dcbdc1c269af1c0096
lgq <lgq@enc.com>
2018-12-13 19:56:21 +0800

    add strconv test.

--- gotest/src/stdlib/strconv_test.go
+++ gotest/src/stdlib/strconv_test.go
@@ -1,16 +1,51 @@
 package stdlib
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
-	"time"
 )
 
 func TestFormatInt(t *testing.T) {
-	var t1 time.Time = time.Now()
-	var tu int64 = t1.Unix()
-	t.Log(t1)
-	t.Log(t1.UnixNano())
-	t.Log(strconv.FormatInt(tu, 10))
-	t.Log(strconv.FormatInt(14, 2))
+	// i to a
+	fmt.Println(strconv.FormatInt(12346, 10))
+	fmt.Println(strconv.FormatInt(12346, 2))
+	fmt.Println(strconv.FormatInt(12346, 8))
+	fmt.Println(strconv.FormatInt(12346, 16))
+	fmt.Println(strconv.Itoa(12346))
+
+	// a to i
+	fmt.Println(strconv.Atoi("123456"))
+	fmt.Println(strconv.ParseInt("-1a2b3d4e", 16, 0))
+	fmt.Println(strconv.ParseInt("0777", 0, 0))
+
+	// float, bool
+	fmt.Println(strconv.FormatFloat(12.3456, 'E', 4, 64))
+	fmt.Println(strconv.FormatFloat(12.3456, 'f', 4, 64))
+	fmt.Println(strconv.FormatFloat(12.3456, 'g', 4, 64))
+	fmt.Println(strconv.FormatFloat(12.3456, 'b', 4, 64))
+	fmt.Println(strconv.FormatBool(true))
+	fmt.Println(strconv.ParseBool("true"))
+	fmt.Println(strconv.ParseFloat("12.3456", 64))
+
+	// quote
+	fmt.Println(strconv.Quote("dasda"))
+	fmt.Println(strconv.QuoteRune('阿'))
+	fmt.Println(strconv.QuoteRuneToASCII('阿'))
+	fmt.Println(strconv.QuoteToASCII("我是黄旭东"))
+	fmt.Println(strconv.QuoteToGraphic("我是黄旭东"))
+
+	// graphic
+	fmt.Println("graphic a: ", strconv.IsGraphic('a'))
+	fmt.Println("graphic 我: ", strconv.IsGraphic('我'))
+	fmt.Println("graphic 😀: ", strconv.IsGraphic('😀'))
+
+	// append
+	buf := make([]byte, 0)
+	buf = strconv.AppendQuote(buf, "我是黄旭东")
+	buf = strconv.AppendQuoteRune(buf, '黄')
+	buf = strconv.AppendBool(buf, true)
+	buf = strconv.AppendInt(buf, 123456, 16)
+	buf = strconv.AppendFloat(buf, 12.3456, 'e', 4, 64)
+	fmt.Println(string(buf))
 }
