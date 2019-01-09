632d846ec4ca5767cf9367a459c4ad4f8ba5bdd2
lgq <lgq@doggy.planet>
2019-01-09 15:24:21 +0800

    websocket echo example.

---
+++ websock/src/websock/echo/server/server.go
@@ -0,0 +1,138 @@
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"html/template"
+	"log"
+	"net/http"
+	"websock/src/websock/echo"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("upgrade error:", err)
+		return
+	}
+	defer echo.Close("server conn", conn)
+	fmt.Println("server local addr:", conn.LocalAddr(), ", remote addr:", conn.RemoteAddr())
+	conn.SetCloseHandler(func(code int, text string) error {
+		fmt.Printf("[server] remote: %s closed.\n", echo.ToString(conn.RemoteAddr()))
+		return nil
+	})
+	for {
+		mt, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read error:", err)
+			break
+		}
+		fmt.Printf("recv: %s, from: %s\n", message, echo.ToString(conn.RemoteAddr()))
+		err = conn.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write error:", err)
+			break
+		}
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(log.Lshortfile)
+	http.HandleFunc("/echo", echoHandler)
+	http.HandleFunc("/", homeHandler)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
+
+// The 'ws = new WebSocket("{{.}}");' code in html is actually a websocket client in browser
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.innerHTML = message;
+        output.appendChild(d);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output"></div>
+</td></tr></table>
+</body>
+</html>
+`))
---
+++ websock/src/websock/echo/util.go
@@ -0,0 +1,23 @@
+package echo
+
+import (
+	"fmt"
+	"io"
+)
+
+func ToString(stringer fmt.Stringer) string {
+	if stringer != nil {
+		return stringer.String()
+	} else {
+		return "<nil>"
+	}
+}
+
+func Close(name string, closer io.Closer) {
+	err := closer.Close()
+	if err != nil {
+		fmt.Println("close", name, "err:", err)
+	} else {
+		fmt.Println(name, "closed")
+	}
+}
