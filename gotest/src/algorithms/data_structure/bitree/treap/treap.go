83210e93daace50dc48527518105fae4ca5faeef
lgq <lgq@enc.com>
2019-06-06 15:07:40 +0800

    implement treap.

--- gotest/src/algorithms/data_structure/bitree/treap/treap.go
+++ gotest/src/algorithms/data_structure/bitree/treap/treap.go
@@ -1,1 +1,250 @@
 package treap
+
+import (
+	"math/rand"
+	"time"
+)
+
+var rander = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+type Treap struct {
+	Root *TreapNode
+}
+
+func NewTreap() *Treap {
+	return &Treap{nil}
+}
+
+type TreapNode struct {
+	left, right, parent *TreapNode
+	ticket              int64
+	key                 int
+	value               interface{}
+}
+
+func (tr *Treap) leftRotate(t *TreapNode) {
+	if t == nil || t.right == nil {
+		return
+	}
+	r := t.right
+	rl := r.left
+	r.left = t
+	t.right = rl
+	if rl != nil {
+		rl.parent = t
+	}
+	if t.parent == nil {
+		tr.Root = r
+	} else if t.parent.left == t {
+		t.parent.left = r
+	} else {
+		t.parent.right = r
+	}
+	r.parent = t.parent
+	t.parent = r
+}
+
+func (tr *Treap) rightRotate(t *TreapNode) {
+	if t == nil || t.left == nil {
+		return
+	}
+	l := t.left
+	lr := l.right
+	l.right = t
+	t.left = lr
+	if lr != nil {
+		lr.parent = t
+	}
+	if t.parent == nil {
+		tr.Root = l
+	} else if t.parent.left == t {
+		t.parent.left = l
+	} else {
+		t.parent.right = l
+	}
+	l.parent = t.parent
+	t.parent = l
+}
+
+func (tr *Treap) Get(key int) *TreapNode {
+	t := tr.Root
+	for t != nil {
+		if key == t.key {
+			return t
+		} else if key < t.key {
+			t = t.left
+		} else {
+			t = t.right
+		}
+	}
+	return nil
+}
+
+func (tr *Treap) Set(key int, v interface{}) {
+	if tr.Root == nil {
+		tr.Root = &TreapNode{nil, nil, nil, rander.Int63(), key, v}
+		return
+	}
+	t := tr.Root
+	for {
+		if key == t.key {
+			t.value = v
+			return
+		} else if key < t.key {
+			if t.left == nil {
+				t.left = &TreapNode{nil, nil, t, rander.Int63(), key, v}
+				tr.fixAfterInsertion(t.left)
+				return
+			}
+			t = t.left
+		} else {
+			if t.right == nil {
+				t.right = &TreapNode{nil, nil, t, rander.Int63(), key, v}
+				tr.fixAfterInsertion(t.right)
+				return
+			}
+			t = t.right
+		}
+	}
+}
+
+func (tr *Treap) Remove(key int) interface{} {
+	node := tr.Get(key)
+	if node == nil {
+		return nil
+	}
+	v := node.value
+	// both null, check ticket of both child.
+	// if left child's ticket is larger, rotate right on node
+	// if right child's ticket is larger, rotate left on node
+	for node.left != nil && node.right != nil {
+		if node.left.ticket > node.right.ticket {
+			tr.rightRotate(node)
+		} else {
+			tr.leftRotate(node)
+		}
+	}
+	// now node has only one son, or nil son
+	var son *TreapNode = nil
+	if node.left != nil {
+		son = node.left
+	} else {
+		son = node.right
+	}
+	if son != nil { // has one son
+		if node.parent == nil { // delete root, son becomes root
+			tr.Root = son
+		} else if node.parent.left == node {
+			node.parent.left = son
+		} else {
+			node.parent.right = son
+		}
+		node.left, node.right, node.parent = nil, nil, nil
+	} else if node.parent == nil { // we are deleting root and root has no child, the tree is cleared
+		tr.Root = nil
+	} else { // both son nil, leaf
+		if node.parent.left == node { // unlink parent
+			node.parent.left = nil
+		} else {
+			node.parent.right = nil
+		}
+		node.parent = nil
+	}
+	return v
+}
+
+func (tr *Treap) fixAfterInsertion(t *TreapNode) {
+	// maintain a big heap
+	// check if there is a violation of big heap until t==root or no violation
+	for t != tr.Root && t.parent.ticket < t.ticket {
+		if t.parent.left == t {
+			tr.rightRotate(t.parent)
+		} else {
+			tr.leftRotate(t.parent)
+		}
+	}
+}
+
+func (t *TreapNode) successor() *TreapNode {
+	if t.right != nil {
+		t = t.right
+		for t.left != nil {
+			t = t.left
+		}
+		return t
+	}
+	p := t.parent
+	for p != nil {
+		if p.left == t {
+			return p
+		}
+		t = p
+		p = p.parent
+	}
+	return nil
+}
+
+func (t *TreapNode) predecessor() *TreapNode {
+	if t.left != nil {
+		t = t.left
+		for t.right != nil {
+			t = t.right
+		}
+		return t
+	}
+	p := t.parent
+	for p != nil {
+		if p.right == t {
+			return p
+		}
+		t = p
+		p = p.parent
+	}
+	return nil
+}
+
+type iter struct {
+	cur *TreapNode
+}
+
+func (tr *Treap) Iter() *iter {
+	first := tr.Root
+	if first == nil {
+		return &iter{first}
+	}
+	for first.left != nil {
+		first = first.left
+	}
+	return &iter{first}
+}
+
+func (tr *Treap) ReverseIter() *iter {
+	last := tr.Root
+	if last == nil {
+		return &iter{last}
+	}
+	for last.right != nil {
+		last = last.right
+	}
+	return &iter{last}
+}
+
+func (it *iter) hasPrev() bool {
+	return it.cur != nil
+}
+
+func (it *iter) prev() *TreapNode {
+	cur := it.cur
+	it.cur = cur.predecessor()
+	return cur
+}
+
+func (it *iter) hasNext() bool {
+	return it.cur != nil
+}
+
+func (it *iter) next() *TreapNode {
+	cur := it.cur
+	it.cur = cur.successor()
+	return cur
+}
--- gotest/src/algorithms/data_structure/bitree/treap/treap_test.go
+++ gotest/src/algorithms/data_structure/bitree/treap/treap_test.go
@@ -1,1 +1,111 @@
 package treap
+
+import (
+	"container/list"
+	"fmt"
+	"math"
+	"testing"
+)
+
+// export GOROOT=/usr/local/go
+// export GOPATH=/home/xzy/golang/gotest:/home/xzy/go
+// go test -c -o /tmp/treaper algorithms/data_structure/bitree/treap
+// go test -run /tmp/treaper -test.cpuprofile ./cpu.out -v algorithms/data_structure/bitree/treap
+// go tool pprof -pdf treap.test cpu.out
+func TestTreapHeight(t *testing.T) {
+	n := (1 << 20) - 1
+	ints := rander.Perm(n)
+	tree := NewTreap()
+	for i := range ints {
+		tree.Set(ints[i], nil)
+	}
+	validateTreap(t, tree)
+	left := make(map[*TreapNode]int)
+	right := make(map[*TreapNode]int)
+	getHeight(tree.Root, left, right)
+	fmt.Println("root left, right:", left[tree.Root], right[tree.Root])
+	fmt.Println("left right map len:", len(left), len(right))
+	maxdiff := float64(0)
+	curdiff := float64(0)
+	alldiff := float64(0)
+	nonleaf := 0
+	for k := range left {
+		curdiff = math.Abs(float64(left[k] - right[k]))
+		maxdiff = math.Max(maxdiff, curdiff)
+		if left[k] == 0 && right[k] == 0 {
+			continue // skip leaf
+		}
+		nonleaf++
+		alldiff += curdiff
+	}
+	fmt.Println("max diff:", maxdiff)
+	fmt.Println("nonleaf:", nonleaf, ", avg diff:", alldiff/float64(nonleaf))
+}
+
+func TestTreapRemove(t *testing.T) {
+	n := (1 << 16) - 1
+	ints := rander.Perm(n)
+	tree := NewTreap()
+	for i := range ints {
+		tree.Set(ints[i], nil)
+	}
+	count := validateTreap(t, tree)
+	if count != n {
+		t.FailNow()
+	}
+	for i := 100; i < 200; i++ {
+		tree.Remove(ints[i])
+		if validateTreap(t, tree) != count-1 {
+			t.Error("error remove")
+		}
+		count--
+	}
+}
+
+func validateTreap(t *testing.T, tree *Treap) int {
+	count := 0
+	queue := list.New()
+	queue.PushBack(tree.Root)
+	for queue.Len() > 0 {
+		cur := queue.Remove(queue.Front()).(*TreapNode)
+		count++
+		if cur.parent != nil {
+			if cur.parent.left == cur && cur.key > cur.parent.key {
+				t.Error("bst left violate")
+			}
+			if cur.parent.right == cur && cur.key < cur.parent.key {
+				t.Error("bst right violate")
+			}
+			if cur.parent.ticket < cur.ticket {
+				t.Error("heap violate")
+			}
+		}
+		if cur.left != nil {
+			queue.PushBack(cur.left)
+		}
+		if cur.right != nil {
+			queue.PushBack(cur.right)
+		}
+	}
+	return count
+}
+
+var max = func(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func getHeight(t *TreapNode, left, right map[*TreapNode]int) int {
+	if t == nil {
+		return 0
+	} else {
+		leftHeight := getHeight(t.left, left, right)
+		rightHeight := getHeight(t.right, left, right)
+		left[t] = leftHeight
+		right[t] = rightHeight
+		return 1 + max(leftHeight, rightHeight)
+	}
+}
