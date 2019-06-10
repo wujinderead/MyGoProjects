cdd1453c383772cef31b9430b47e47111b702b20
xh <xzy@node1>
2019-06-10 23:22:36 +0800

    warnsdorff algorithm for knight tour.

--- gotest/src/algorithms/backtracking/knight_tour.go
+++ gotest/src/algorithms/backtracking/knight_tour.go
@@ -6,10 +6,6 @@ import (
 
 var deltas = [][]int{{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}}
 
-// the order of solution would greatly affect the result
-// use the following delta sequence for size 8, it take hours to get the result
-// var deltas = [][]int{{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}}
-
 func knightTour(size int) {
 	board := make([][]int, size, size)
 	for i := 0; i < size; i++ {
@@ -45,4 +41,52 @@ func knightTourNext(board [][]int, curx, cury, count int) bool {
 
 func canGoto(board [][]int, x, y int) bool {
 	return x >= 0 && x < len(board) && y >= 0 && y < len(board) && board[x][y] == 0
+}
+
+func getDegree(board [][]int, x, y int) int {
+	unvisitedAdjacency := 0
+	for i := range deltas {
+		dx := deltas[i][0]
+		dy := deltas[i][1]
+		if canGoto(board, x+dx, y+dy) {
+			unvisitedAdjacency++
+		}
+	}
+	return unvisitedAdjacency
+}
+
+// Warnsdorff’s algorithm for Knight’s tour problem:
+// start from any initial position of the knight on the board,
+// always move to an adjacent, unvisited square with minimal degree
+// (minimum number of unvisited adjacent). it will guarantee to get a solution.
+// On an 8 × 8 board, there are exactly 26,534,728,821,064 directed closed tours.
+func knightTourWarnsdorff(size int) {
+	board := make([][]int, size, size)
+	for i := 0; i < size; i++ {
+		board[i] = make([]int, size, size)
+	}
+
+	board[0][0] = 1
+	prevx, prevy := 0, 0
+	for i := 2; i < size*size; i++ {
+		minDegree := 8
+		minx := 0
+		miny := 0
+		for i := range deltas {
+			dx := deltas[i][0]
+			dy := deltas[i][1]
+			if canGoto(board, prevx+dx, prevy+dy) {
+				curDegree := getDegree(board, prevx+dx, prevy+dy)
+				if curDegree <= minDegree {
+					minDegree = curDegree
+					minx = prevx + dx
+					miny = prevy + dy
+				}
+			}
+		}
+		board[minx][miny] = i
+		prevx = minx
+		prevy = miny
+	}
+	fmt.Println(board)
 }
--- gotest/src/algorithms/backtracking/knight_tour_test.go
+++ gotest/src/algorithms/backtracking/knight_tour_test.go
@@ -9,9 +9,20 @@ import (
 //go test -c -o /tmp/aaa algorithms/backtracking
 //go test -run /tmp/aaa algorithms/backtracking
 //go tool test2json -t /tmp/aaa -test.v -test.run ^TestKnightRun$
-func TestKnightRun(t *testing.T) {
+
+// the order of delta would greatly affect the result
+// use the following delta sequence for size 8, it take hours to get the result
+// var deltas = [][]int{{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}}
+func TestKnightTour(t *testing.T) {
 	for i := 3; i <= 8; i++ {
 		fmt.Println("size", i, ":")
 		knightTour(i)
+	}
+}
+
+func TestKnightTourWarnsdorff(t *testing.T) {
+	for i := 3; i <= 20; i++ {
+		fmt.Println("size", i, ":")
+		knightTourWarnsdorff(i)
 	}
 }
