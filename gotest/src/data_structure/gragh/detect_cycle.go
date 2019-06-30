c32f098d1a88ee8696e091ecc79f4620c17437de
xh <xzy@node1>
2019-06-30 23:02:45 +0800

    detect cycle.

--- gotest/src/data_structure/gragh/detect_cycle.go
+++ gotest/src/data_structure/gragh/detect_cycle.go
@@ -2,8 +2,9 @@ package gragh
 
 import "fmt"
 
+// assume no parallel edge between two vertices
 func detectCycleUndirectedDfs(g *graph) bool {
-	// dfs to store nods in stack, assume no parallel edge between two verticesß
+	// use stack to perform dfs
 	stack := make([]int, g.n)
 	parent := make([]int, g.n)
 	stackind := -1
@@ -34,11 +35,61 @@ func detectCycleUndirectedDfs(g *graph) bool {
 	return false
 }
 
-func detectCycleDirectedDfs(g *graph) {
-
+func detectCycleDirectedDfs(g *graph) bool {
+	// use stack to perform dfs
+	instack := make([]bool, g.n)
+	visited := make([]bool, g.n)
+	for i := 0; i < g.n; i++ {
+		if !visited[i] && detectCycleDirectedUtil(g, i, visited, instack) {
+			return true
+		}
+	}
+	return false
 }
 
-// union is for undirected graph
-func detectCycleUnionFind(g *graph) {
+func detectCycleDirectedUtil(g *graph, cur int, visited, instack []bool) bool {
+	visited[cur] = true
+	instack[cur] = true
+	// defer func(){instack[cur]=false}()   // can use defer here, buf defer will affect performance
+	for nb := g.adjacency[cur]; nb != nil; nb = nb.next {
+		// if cur has a adjacent vertex that is already in the stack
+		// then there is a cycle
+		fmt.Println(cur, "->", nb.id)
+		if instack[nb.id] {
+			fmt.Println("back edge:", cur, nb.id)
+			// set instack[cur] to false when leave current func call
+			instack[cur] = false
+			return true
+		}
+		if !visited[nb.id] && detectCycleDirectedUtil(g, nb.id, visited, instack) {
+			instack[cur] = false
+			return true
+		}
+	}
+	instack[cur] = false // set in stack to false
+	return false
+}
 
+// union-find is for undirected graph
+// add edges to disjoint set one by one, if two vertices of an edge is in the same set
+// then there is a cycle
+func detectCycleUnionFind(g *graph) bool {
+	disjointset := make([]int, g.n)
+	for i := 0; i < g.n; i++ {
+		disjointset[i] = -1
+	}
+	for i := 0; i < g.n; i++ {
+		for nb := g.adjacency[i]; nb != nil; nb = nb.next {
+			if i < nb.id { // only process edge for once
+				x := find(disjointset, i)
+				y := find(disjointset, nb.id)
+				fmt.Println(i, nb.id, x, y)
+				if x == y {
+					return true
+				}
+				union(disjointset, x, y)
+			}
+		}
+	}
+	return false
 }
--- gotest/src/data_structure/gragh/detect_cycle_test.go
+++ gotest/src/data_structure/gragh/detect_cycle_test.go
@@ -12,13 +12,14 @@ func TestDetectCycleUndirected(t *testing.T) {
 }
 
 func TestDetectCycleDirected(t *testing.T) {
-	g := newUndirectedGraph(8, [][]int{{0, 1}, {1, 2}, {2, 3}, {3, 4},
-		{4, 5}, {5, 0}, {0, 6}, {1, 7}})
-	fmt.Println(detectCycleUndirectedDfs(g))
+	g := newDirectedGraph(8, [][]int{{1, 0}, {0, 2}, {0, 6}, {2, 3},
+		{3, 0}, {1, 4}, {4, 5}, {5, 3}, {5, 5}})
+	fmt.Println(detectCycleDirectedDfs(g))
 }
 
 func TestDetectCycleUnionFind(t *testing.T) {
 	g := newUndirectedGraph(8, [][]int{{0, 1}, {1, 2}, {2, 3}, {3, 4},
 		{4, 5}, {5, 0}, {0, 6}, {1, 7}})
-	fmt.Println(detectCycleUndirectedDfs(g))
+	g.print()
+	fmt.Println(detectCycleUnionFind(g))
 }
