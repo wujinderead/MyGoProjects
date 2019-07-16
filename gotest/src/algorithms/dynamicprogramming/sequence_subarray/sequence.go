eeb724125f277d3a4c65b08c9bb40b436689cf90
lgq <lgq@enc.com>
2019-07-16 18:21:27 +0800

    lis problem.

--- gotest/src/algorithms/dynamicprogramming/sequence_subarray/sequence.go
+++ gotest/src/algorithms/dynamicprogramming/sequence_subarray/sequence.go
@@ -1,6 +1,22 @@
 package longest
 
 func longestIncreasingSubsequence(seq []int) int {
-	// todo
-	return 0
+	// for seq[i], check if seq[i] (0≤k<i) > seq[k], if seq[i] > seq[k], seq[i] = max(seq[i], seq[k]+1)
+	if len(seq) < 0 {
+		return 0
+	}
+	maxlis := 1
+	lis := make([]int, len(seq))
+	for i := 0; i < len(seq); i++ {
+		lis[i] = 1
+		for j := 0; j < i; j++ {
+			if seq[j] < seq[i] && lis[i] < lis[j]+1 {
+				lis[i] = lis[j] + 1
+			}
+			if maxlis < lis[i] {
+				maxlis = lis[i]
+			}
+		}
+	}
+	return maxlis
 }
--- gotest/src/algorithms/dynamicprogramming/sequence_subarray/sequnce_test.go
+++ gotest/src/algorithms/dynamicprogramming/sequence_subarray/sequnce_test.go
@@ -1,1 +1,21 @@
 package longest
+
+import (
+	"testing"
+)
+
+func TestLongestIncreasingSubsequence(t *testing.T) {
+	cases := [][]interface{}{
+		{[]int{10, 22, 9, 33, 21, 50, 41, 60, 80}, 6},
+		{[]int{50, 3, 10, 7, 40, 80}, 4},
+		{[]int{3, 2}, 1},
+	}
+	for i := range cases {
+		seq := cases[i][0].([]int)
+		expect := cases[i][1].(int)
+		re := longestIncreasingSubsequence(seq)
+		if re != expect {
+			t.Error(seq, expect, re)
+		}
+	}
+}
