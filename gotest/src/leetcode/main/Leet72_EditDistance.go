9d579345ac7d6b28bafa3dfc0673b3f7107bd70f
xh <xzy@node1>
2019-07-31 23:25:08 +0800

    leet 95, 96.

---
+++ gotest/src/leetcode/main/Leet72_EditDistance.go
@@ -0,0 +1,21 @@
+package main
+
+// https://leetcode.com/problems/edit-distance/
+
+// Given two words word1 and word2, find the minimum number of
+// operations required to convert word1 to word2.
+// You have the following 3 operations permitted on a word:
+// Insert a character
+// Delete a character
+// Replace a character
+// Example:
+//   Input: word1 = "horse", word2 = "ros"
+//   Output: 3
+//   Explanation:
+//     horse -> rorse (replace 'h' with 'r')
+//     rorse -> rose (remove 'r')
+//     rose -> ros (remove 'e')
+func minDistance(word1 string, word2 string) int {
+	// todo
+	return 0
+}
---
+++ gotest/src/leetcode/main/Leet95_UniqueBinarySearchTreesII.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+// https://leetcode.com/problems/unique-binary-search-trees-ii/
+
+// Given an integer n, generate all structurally unique
+// BST's (binary search trees) that store values 1 ... n.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+var nilTree = []*TreeNode{nil}
+
+func generateTrees(n int) []*TreeNode {
+	if n == 0 {
+		return []*TreeNode{}
+	}
+	arr := make([]int, n)
+	for i := range arr {
+		arr[i] = i + 1
+	}
+	return generateTreeArray(arr)
+}
+
+func generateTreeArray(arr []int) []*TreeNode {
+	if len(arr) == 0 {
+		return nilTree
+	}
+	trees := make([]*TreeNode, 0)
+	for i := 0; i < len(arr); i++ {
+		// arr[i] as root
+		lefts := generateTreeArray(arr[0:i])
+		rights := generateTreeArray(arr[i+1:])
+		for _, left := range lefts {
+			for _, right := range rights {
+				trees = append(trees, &TreeNode{arr[i], left, right})
+			}
+		}
+	}
+	return trees
+}
+
+func main() {
+	trees := generateTrees(4)
+	for _, tree := range trees {
+		fmt.Println(tree.Val)
+	}
+}
