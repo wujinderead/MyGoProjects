eb8284fc938d20cc936878edb20dabadfa33f221
lgq <lgq@enc.com>
2019-08-15 18:00:50 +0800

    http not found, timeout.

--- gotest/neter/httper/testServe.go
+++ gotest/neter/httper/testServe.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
-	testHandleServe()
+	//testHandleServe()
+	testNotFoundTimeout()
 }
 
 func testHandleServe() {
@@ -42,4 +45,28 @@ func testHandleServe() {
 	// http get :8080/
 	http.Handle("/", http.HandlerFunc(indexHandler))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func testNotFoundTimeout() {
+	// response '404 Not Found'
+	http.HandleFunc("/notfound", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("haha not found. "))
+		http.NotFound(w, r)
+	}))
+	// a time-consuming handler
+	timeConsumingHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		workTime := rand.Intn(10)
+		log.Println("work time:", workTime)
+		time.Sleep(time.Duration(workTime) * time.Second)
+		_, _ = w.Write([]byte("done in "))
+		_, _ = w.Write([]byte{byte(workTime) + '0'})
+		_, _ = w.Write([]byte(" seconds."))
+	})
+	// timeout handler response '503 Service Unavailable' if handler's process time exceeds the specified timeout
+	http.Handle("/timeout", http.TimeoutHandler(timeConsumingHandler, 5*time.Second, "timeout"))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func testProxy() {
+	http.ProxyFromEnvironment()
 }
