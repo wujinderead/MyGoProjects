25468d4fe57e77f9a7abca51b2e0a38664d90fe8
lgq <lgq@enc.com>
2019-08-19 16:45:59 +0800

    test ed25519 generate key.

--- ecc/util/x25519_ed25519_test.go
+++ ecc/util/x25519_ed25519_test.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"bytes"
 	"crypto"
 	"crypto/rand"
 	"crypto/sha512"
@@ -11,39 +10,49 @@ import (
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/curve25519"
 	"golang.org/x/crypto/ed25519"
-	"math/big"
 	"reflect"
 	"testing"
 )
 
-// curve25519
-func TestCurve25519(t *testing.T) {
-	var priv1, priv2, pub1, pub2, share1, share2 [32]byte
-	_, _ = rand.Read(priv1[:])
-	_, _ = rand.Read(priv2[:])
-	fmt.Printf("priv1: %x\n", priv1)
-	fmt.Printf("priv2: %x\n", priv2)
-	curve25519.ScalarBaseMult(&pub1, &priv1)
-	curve25519.ScalarBaseMult(&pub2, &priv2)
-	fmt.Printf("pub1: %x\n", pub1)
-	fmt.Printf("pub2: %x\n", pub2)
-	curve25519.ScalarMult(&share2, &priv2, &pub1)
-	curve25519.ScalarMult(&share1, &priv1, &pub2)
-	fmt.Printf("share1: %x\n", share1)
-	fmt.Printf("share2: %x\n", share2)
-	assert.Equal(t, share1, share2, "share equal")
+// https://tools.ietf.org/html/rfc7748
+func TestX25519Rfc7748PrivPub(t *testing.T) {
+	// test vector from rfc7748
+	it1, _ := hex.DecodeString("422c8e7a6227d7bca1350b3e2bb7279f7897b87bb6854b783c60e80311ae3079")
+	it1000, _ := hex.DecodeString("684cf59ba83309552800ef566f2f4d3c1c3887c49360e3875f2eb94d99532c51")
+	it1000000, _ := hex.DecodeString("7c3911e0ab2586fd864497297e575e6f3bc601c0883c30df5f4dd2d24f665424")
 
-	// the order of in and base is important, the following is wrong
-	curve25519.ScalarMult(&share2, &pub1, &priv2)
-	curve25519.ScalarMult(&share1, &pub2, &priv1)
-	fmt.Printf("share1: %x\n", share1)
-	fmt.Printf("share2: %x\n", share2)
-	assert.NotEqual(t, share1, share2, "share not equal")
+	// initialize base and scala both as [9 0 0 0...]
+	var base, scala, prod [32]byte
+	base[0] = 9
+	scala[0] = 9
+
+	// do 1 iteration
+	curve25519.ScalarMult(&prod, &scala, &base)
+	fmt.Printf("1 iteration: %x\n", prod[:])
+	assert.Equal(t, it1, prod[:])
+
+	// do 1000 iteration
+	for i := 2; i <= 1000; i++ {
+		// use product as new scala, use old scala as new base
+		base, prod, scala = scala, base, prod
+		curve25519.ScalarMult(&prod, &scala, &base)
+	}
+	fmt.Printf("1000 iteration: %x\n", prod[:])
+	assert.Equal(t, it1000, prod[:])
+
+	// do 1000000 iteration, this takes dozens of seconds time
+	for i := 1001; i <= 1000000; i++ {
+		// use product as new scala, use old scala as new base
+		base, prod, scala = scala, base, prod
+		curve25519.ScalarMult(&prod, &scala, &base)
+	}
+	fmt.Printf("1000000 iteration: %x\n", prod[:])
+	assert.Equal(t, it1000000, prod[:])
 }
 
-// https://tools.ietf.org/html/rfc7748
-func TestX25519Rfc7748(t *testing.T) {
+func TestX25519Rfc7748DiffieHellman(t *testing.T) {
 	// test golang.org/x/crypto/curve25519 package
+	// test vectors from in rfc7748, all in little-endian
 	scala1, _ := hex.DecodeString("77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a")
 	scala2, _ := hex.DecodeString("5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb")
 	pub1Ex, _ := hex.DecodeString("8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a")
@@ -66,24 +75,8 @@ func TestX25519Rfc7748(t *testing.T) {
 	assert.Equal(t, share1[:], share2[:], "share 1,2 equal")
 
 	// test ecc package
-	process := func(scala []byte) []byte {
-		e := make([]byte, len(scala))
-		copy(e, scala)
-		e[0] &= 248
-		e[31] &= 127
-		e[31] |= 64
-		return e
-	}
-	reverse := func(b []byte) []byte {
-		i, j := 0, len(b)-1
-		for i < j {
-			b[i], b[j] = b[j], b[i]
-			i++
-			j--
-		}
-		return b
-	}
-	// ecc scala multiplication input is big-endian, rfc7748 input and output are little-endian
+	// 'golang.org/x/crypto/curve25519' input and output are little-endian
+	// 'ecc' scala multiplication input is big-endian
 	pub1Ecc := ecc.Curve25519().ScalaMultBase(reverse(process(scala1)))
 	pub2Ecc := ecc.Curve25519().ScalaMultBase(reverse(process(scala2)))
 	share1Ecc := ecc.Curve25519().ScalaMult(pub1Ecc, reverse(process(scala2)))
@@ -96,6 +89,16 @@ func TestX25519Rfc7748(t *testing.T) {
 	assert.Equal(t, reverse(pub2Ecc.X.Bytes()), pub2Ex, "pub2 equal")
 	assert.Equal(t, reverse(share1Ecc.X.Bytes()), shareE, "share equal")
 	assert.Equal(t, share1Ecc.X.Bytes(), share2Ecc.X.Bytes(), "share 1,2 equal")
+
+	// test scala multiplication in projective coordinates
+	pub1Pro := ecc.Curve25519().ScalaMultBaseProjective(reverse(process(scala1)))
+	pub2Pro := ecc.Curve25519().ScalaMultBaseProjective(reverse(process(scala2)))
+	share1Pro := ecc.Curve25519().ScalaMultProjective(pub1Ecc, reverse(process(scala2)))
+	share2Pro := ecc.Curve25519().ScalaMultProjective(pub2Ecc, reverse(process(scala1)))
+	assert.Equal(t, reverse(pub1Pro.X.Bytes()), pub1Ex, "pub1 equal")
+	assert.Equal(t, reverse(pub2Pro.X.Bytes()), pub2Ex, "pub2 equal")
+	assert.Equal(t, reverse(share1Pro.X.Bytes()), shareE, "share equal")
+	assert.Equal(t, share1Pro.X.Bytes(), share2Pro.X.Bytes(), "share 1,2 equal")
 }
 
 func TestEd25519(t *testing.T) {
@@ -119,51 +122,52 @@ func TestEd25519(t *testing.T) {
 	fmt.Println(ed25519.Verify(ed25519.PublicKey(pub), msg, sign))
 }
 
-// curve25519 base_point (bu=9, bv)   *   secret_K  =  (U, ?)        U: curve25519 public key
-//                           ||                          ||
-// ed25519 base_point  (bx, by=4/5)   *   secret_K  =  (?, Y)        Y: ed25519 public key
-// y = (u-1)/(u+1)
-// u = (1+y)/(1-y)
-func TestCurve25519AndEd25519Arithmetic(t *testing.T) {
-	reader := rand.Reader
-	seed := make([]byte, ed25519.SeedSize)
-	n, err := reader.Read(seed)
-	if n < 32 || err != nil {
-		fmt.Println("error read seed: ", err)
-		t.FailNow()
+func TestEd25519GenerateKey(t *testing.T) {
+	var seed [32]byte
+	_, _ = rand.Read(seed[:])
+	privKey := ed25519.NewKeyFromSeed(seed[:])
+
+	// ed25519 key is seed + public key
+	fmt.Println("seed:", hex.EncodeToString(privKey[:32]))
+	fmt.Println("pubK:", hex.EncodeToString(privKey[32:]))
+
+	// re-perform a key generate process
+	h := sha512.New()
+	h.Write(seed[:])
+	digest := h.Sum(nil)
+
+	var priK = digest[:32]
+	priK[0] &= 248
+	priK[31] &= 127
+	priK[31] |= 64
+	pubPoint := ecc.Ed25519().ScalaMultBaseProjective(reverse(priK))
+	y := reverse(pubPoint.Y.Bytes())
+	x := reverse(pubPoint.X.Bytes())
+	fmt.Println("y   :", hex.EncodeToString(y))
+	fmt.Println("x   :", x[0])
+	// copy the least significant bit of pubPoint.X to the most significant bit of pubPoint.Y
+	if x[0]&1 == 1 { // the least significant bit of pubPoint.X is 1
+		y[31] |= 0x80
+	} else {
+		y[31] &= 0x7f
 	}
-	fmt.Printf("seed: %x\n", seed)
-	dgst := sha512.Sum512(seed)
-	var mltr [32]byte
-	copy(mltr[:], dgst[:])
-	fmt.Printf("dgst: %x\n", dgst[:])
-	fmt.Printf("mltr: %x\n", mltr[:])
-	privKey := ed25519.NewKeyFromSeed(seed)
-	privs := []byte(privKey)[0:32]
-	pubs := []byte(privKey)[32:]
-	fmt.Printf("priv: %x\n", privs)
-	fmt.Printf("pubs: %x\n", pubs)
+	fmt.Println("newy:", hex.EncodeToString(y))
+	assert.Equal(t, y, []byte(privKey[32:]), "public key equal")
+}
 
-	var rslt [32]byte
-	curve25519.ScalarBaseMult(&rslt, &mltr)
-	fmt.Printf("rslt: %x\n", rslt)
-
-	// curve25519 and ed25519 are little-endian, while big.Int is big-endian.
-	// so every time we see a big.Int, reverse it.
-	reverse := func(arr []byte) []byte {
-		for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-		return arr
+func process(scala []byte) []byte {
+	e := make([]byte, len(scala))
+	copy(e, scala)
+	e[0] &= 248
+	e[31] &= 127
+	e[31] |= 64
+	return e
+}
+func reverse(b []byte) []byte {
+	a := make([]byte, len(b))
+	copy(a, b)
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
 	}
-
-	y := Curve25519XToEd25519Y(new(big.Int).SetBytes(reverse(rslt[:])))
-	fmt.Printf("y   : %x\n", reverse(y.Bytes()))
-
-	// when reverse(pubs) equals y, u=u1; reverse(pubs) and y may have minor difference
-	u := Ed25519YToCurve25519X(new(big.Int).SetBytes(reverse(pubs)))
-	u1 := Ed25519YToCurve25519X(y)
-	fmt.Printf("u   : %x\n", reverse(u.Bytes()))
-	fmt.Printf("u1  : %x\n", reverse(u1.Bytes()))
-	fmt.Println(bytes.Equal(pubs, reverse(y.Bytes())), bytes.Equal(rslt[:], reverse(u.Bytes())))
+	return a
 }
